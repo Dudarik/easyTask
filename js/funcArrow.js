4700e033d0446c776778ab51d2c4fc3b8a7b57fe
Dudarik <dudarik@yandex.ru>
2020-01-12 16:48:57 +0500

    add some arrow func

--- js/funcArrow.js
+++ js/funcArrow.js
@@ -1,3 +1,9 @@
+/*Стрелочные функции:
+
+Не имеют this.
+Не имеют arguments.
+Не могут быть вызваны с new.*/
+
 let sum = (a, b) =>  a + b;
 
 let double = n => n * 2;
@@ -27,3 +33,31 @@ ask2 ('a', () => console.log('y'), () => console.log('n'));
 let ask3 = (q, y, n) => confirm(q) ? y() : n();
 ask3 ('a', () => console.log('y'), () => console.log('n'));
 
+
+
+let group ={
+    title: "Our Group",
+    students: ['Alex', 'John', 'Natasha'],
+
+    showList(){
+        this.students.forEach(
+            student => console.log(`${this.title}:${student}`)            
+        );
+    }
+};
+
+group.showList();
+
+
+function defer(f, ms) {
+    return function () {
+        setTimeout(() => f.apply(this, arguments), ms)        
+    };
+}
+
+function sayHi(who) {
+    console.log(`Hello, ${who}`);    
+}
+
+let sayHiDeffered = defer(sayHi, 2000);
+sayHiDeffered('ALex');
